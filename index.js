f645326f6df5cfc47838a593923ff9b7ba29feee
LukaKoporec <luka.koporec123@gmail.com>
2021-10-12 17:36:53 +0200

    Node.js-test

---
+++ index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var MySupervillainName = supervillains.random();
+var MySuperheroName = superheroes.random();
+
+console.log(MySuperheroName);
+console.log(MySupervillainName);
